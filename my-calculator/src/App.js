e9ba8c05f430af44ec24f83a3d5ba09c3d639d1e
T_compute2018 <72470140+Tcompute2018@users.noreply.github.com>
2021-12-13 13:26:10 -0600

    updated

--- my-calculator/src/App.js
+++ my-calculator/src/App.js
@@ -1,12 +1,32 @@
+import { useState } from 'react';
+
 
 function App() {
+
+	const [ calc, setCalc ] = useState ("");
+	const [result, setResult] = useState ("");
+
+	const ops = ['/','*','+','-','.'];
+
+	const updateCalc = value =>{
+		if (ops.includes(value) && calc === '' || 
+		    ops.includes(value) && ops.includes(calc.slice(-1)) 
+			){
+				return;
+			}
+		setCalc(calc + value);
+	}
 
 	const createDigits = () => {
 		const digit = [];
 
 		for (let i = 1; i < 10 ; i++){
 				digit.push(
-					<button key = {i}> {i} </button>
+					<button 
+						onClick = {()=> updateCalc(i.toString())}
+						key = {i}> 
+						{i} 
+					</button>
 				)
 		}
 		return digit;
@@ -16,23 +36,24 @@ function App() {
 		<div className="App">
 			<div className ="calculator"> 
 				<div className = "display">
-					<span> (0)</span> 0
+					{ result ? <span> (0)</span> : ''} 
+					{ calc || "0" }
 				</div>
 
 				<div className = "operator">
-					<button>/</button>
-					<button>*</button> 
-					<button>+</button> 
-					<button>-</button>
+					<button onClick = {()=> updateCalc('/')}>/</button>
+					<button onClick = {()=> updateCalc('*')}>*</button> 
+					<button onClick = {()=> updateCalc('+')}>+</button> 
+					<button onClick = {()=> updateCalc('-')}>-</button>
 
 					<button>DEL</button>
 				</div>
 
 				<div className = "digits">
 					{ createDigits() }
-					<button>0</button>
-					<button>.</button> 
-					<button>=</button> 
+					<button onClick = {()=> updateCalc('0')}>0</button>
+					<button onClick = {()=> updateCalc('.')}>.</button> 
+					<button onClick = {()=> updateCalc('=')}>=</button>
 					
 				</div>
 				
